92af8c52700ff3acf08b27e7795f07221e9075ce
Laura Voss <lauravoss@posteo.de>
2022-04-25 16:51:28 +0100

    complete challenge #06: adding maximum balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,11 +2,16 @@ class Oystercard
 
   attr_reader :balance
   
+  MAXIMUM_CAPACITY = 90
+
   def initialize
     @balance = 0
   end
 
   def top_up(money)
+    fail "Maximum balance of #{MAXIMUM_CAPACITY} exceeded" if balance + money > MAXIMUM_CAPACITY
     @balance += money
   end
+
+ 
 end
