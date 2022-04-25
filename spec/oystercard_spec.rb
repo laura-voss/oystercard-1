e4a4a444f418b4f8cc0dd22596d982ee2e85ca9c
Laura Voss <lauravoss@posteo.de>
2022-04-25 15:08:08 +0100

    complete challenge #04: add balance to oystercard

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,12 @@
+require 'oystercard'
+
 describe Oystercard do
+  it 'initializes new card' do
+    expect(Oystercard.new).to be_an_instance_of(Oystercard)
+  end
+
+  it 'sets new card to 0 balance' do 
+    new_oyster = Oystercard.new
+    expect(new_oyster.balance).to eq(0)
+  end
 end
